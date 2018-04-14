2f23a523c398dfe09a14ff6d9a2203b3b0b0bada
ZeldaZocker <34456207+ZeldaZocker@users.noreply.github.com>
2018-04-15 00:34:13 +0200

    Test

--- Assets/Scripts/Anti-Cheat/BanManager.cs
+++ Assets/Scripts/Anti-Cheat/BanManager.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using CodeStage.AntiCheat.ObscuredTypes;
 using CodeStage.AntiCheat;
+using UnityEngine.SceneManagement;
 
-public class BanManager : MonoBehaviour {
+public class BanManager : MonoBehaviour
+{
 
-	//Zum initialisieren
-	void Start () {
+    //Zum initialisieren
+    //void Start () {
 
-        if (ObscuredPrefs.GetInt("banned") != 1)
-            ObscuredPrefs.SetInt("banned", 0);
+    //  if (ObscuredPrefs.GetInt("banned") != 1)
+    //      ObscuredPrefs.SetInt("banned", 0);
 
-        if (ObscuredPrefs.GetInt("banned") == 1)
-            Application.Quit();
+    // if (ObscuredPrefs.GetInt("banned") == 1)
+    //     Application.Quit();
+    // }
+    private void Update()
+    {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
     }
-    
+
 }
