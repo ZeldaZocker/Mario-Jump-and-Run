ee19058844749109698182d95a9b8b798b0f3ac4
ZeldaZocker <34456207+ZeldaZocker@users.noreply.github.com>
2018-04-15 01:01:07 +0200

    Some testing

--- Assets/OpenTerminal/Scripts/TerminalStrings.cs
+++
@@ -1,6 +0,0 @@
-public class TerminalStrings
-{
-    public const string COMMAND_NOT_FOUND = "Befehl nicht gefunden! Benutze \"help\" um eine Liste der verf�gbaren Befehlen zu erhalten!";
-    public const string MULTIPLE_COMMAND_NAMES = "Mehrere Befehle sind mit folgendem definiert: ";
-    public const string ARGUMENT_COUNT_MISSMATCH = "Funktion {0} ben�tigt {1} Argumente. Angegebene Argumente: {2}";
-}
